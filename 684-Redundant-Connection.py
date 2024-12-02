edcc5f0a98a1a6a611f3a76a5d93336e7e852f1b
Adi <71416354+kana2001@users.noreply.github.com>
2024-12-02 16:38:55 -0500

    [684] [Time Beats: 59.58%] [Memory Beats: 19.40%] - LeetPush

--- 684-Redundant-Connection.py
+++ 684-Redundant-Connection.py
@@ -1,30 +1,33 @@
 class UF:
     def __init__(self, size):
         self.par = [i for i in range(size)]
+        self.rank = [1] * size 
     def find(self, x):
-        res = x
-        if res != self.par[x]:
-            res = self.find(self.par[x])
-        return res
+        if x != self.par[x]:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
     def union(self, x, y):
-        if self.find(x) == self.find(y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
             return False
-        self.par[self.find(x)] = self.par[self.find(y)]
+
+        if self.rank[root_x] > self.rank[root_y]:
+            self.par[root_y] = root_x
+        elif self.rank[root_x] < self.rank[root_y]:
+            self.par[root_x] = root_y
+        else:
+            self.par[root_y] = root_x
+            self.rank[root_x] += 1
         return True
 
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        nodes = set()
-        for x,y in edges:
-            nodes.add(x)
-            nodes.add(y)
-        uf = UF(len(nodes) + 1)
-
-        res = []
+        uf = UF(len(edges) + 1) 
         for x,y in edges:
             if not uf.union(x,y):
-                res = [x,y]
-        return res
+                return [x,y]
 
         
